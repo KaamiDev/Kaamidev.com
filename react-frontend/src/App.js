995a78056635355ed1de0d00b388f94db77eb6c8
Kaami Dev <kaami@kaamidev.com>
2020-08-22 21:35:20 -0400

    created and styled homepage

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
 
+import profileImg from './profile.jpg';
+
 function App() {
 	return (
-		<div>
-			<p>Hello World!</p>
+		<div id="main-card" className="shadow">
+			<div id="start-page">
+				<div className="start-heading">
+					<img src={profileImg} className="shadow" />
+					<h1>KaamiDev</h1>
+					<h3>Full Stack Web Developer</h3>
+				</div>
+				<div className="nav-full shadow2">
+					<ul>
+						<li>Home</li>
+						<li>Projects</li>
+						<li>Contant</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
