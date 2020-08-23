06c2abbb2a63ba814c422a14323eb80157221854
Kaami Dev <kaami@kaamidev.com>
2020-08-23 18:53:09 -0400

    created seperate component files

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -5,24 +5,7 @@ import './App.css';
 import profileImg from './profile.jpg';
 
 function App() {
-	return (
-		<div id="main-card" className="shadow">
-			<div id="start-page">
-				<div className="start-heading">
-					<img src={profileImg} alt="profile pic" className="shadow" />
-					<h1>KaamiDev</h1>
-					<h3>Full Stack Web Developer</h3>
-				</div>
-				<div className="nav-full shadow2">
-					<ul>
-						<li>Home</li>
-						<li>Projects</li>
-						<li>Contact</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	);
+	return <div id="main-card" className="shadow" />;
 }
 
 export default App;
---
+++ react-frontend/src/components/Start.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Start = () => {
+	return (
+		<div id="start-page">
+			<div className="start-heading">
+				<img src={profileImg} alt="profile pic" className="shadow" />
+				<h1>KaamiDev</h1>
+				<h3>Full Stack Web Developer</h3>
+			</div>
+			<div className="nav-full shadow2">
+				<ul>
+					<li>Home</li>
+					<li>Projects</li>
+					<li>Contact</li>
+				</ul>
+			</div>
+		</div>
+	);
+};
+
+export default Start;
